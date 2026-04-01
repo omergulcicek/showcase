2326da057d58108aa30c04aaf1ca9280cb83b138
omergulcicek <iletisim@omergulcicek.com>
2026-04-01 14:52:57 +0300

    Update _meta.js files for English and Turkish versions to disable timestamp property, preventing prerender issues on Vercel shallow clones.

--- content/en/_meta.js
+++ content/en/_meta.js
@@ -4,6 +4,8 @@ export default {
       sidebar: false,
       navbar: false,
       breadcrumb: false,
+      // Disable git-based "last updated" (avoids prerender issues on Vercel shallow clones / bad timestamps).
+      timestamp: false,
     },
   },
   index: {
